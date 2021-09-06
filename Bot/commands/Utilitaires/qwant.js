7a6ac163395168092ee3e5739bd2daa7906d5215
Baptiste <b.a.p.t.s.t.e7@gmail.com>
2021-09-07 01:06:11 +0200

    Préparation des commandes : - Ajout des fichiers - Ajout des constantes d'aide/description/détails

---
+++ Bot/commands/Utilitaires/qwant.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-empty-function */
+/* eslint-disable no-unused-vars */
+const {MESSAGES} = require('../../starterpack/constants')
+
+module.exports.run = async (client, message, args, settings, dbUser) => {
+}
+module.exports.help = MESSAGES.Commandes.Utilitaires.QWANT;
