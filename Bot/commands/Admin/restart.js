18cdce867086d99507e834f178cfaa816c016f6e
Redemption07 <b.a.p.t.s.t.e7@gmail.com>
2022-04-02 01:20:28 +0200

    no message

--- Bot/commands/Admin/restart.js
+++ Bot/commands/Admin/restart.js
@@ -1,8 +1,8 @@
 const {MESSAGES} = require('../../starterpack/constants')
 
 module.exports.run = async (client, message) => {
-    if (!client.config.OWNERS_ID.includes(message.author.id)) return message.reply("seul les owners du bot peuvent éxécuter cette commande !");
-    await client.channels.cache.get(client.config.CHANNELLOGID).send({embeds: [{title: 'Veuillez patienter, le bot redémarre...', color: 'GOLD'}]})
+    if (!client.config.OWNERS_ID.includes(message.author.id)) return message.reply("Seuls les owners du bot peuvent exécuter cette commande !");
+    await client.channels.cache.get(client.config.CHANNELLOGID).send({embeds: [{description: 'Veuillez patienter, le bot redémarre...', color: 'GOLD'}]})
     process.exit()
 }
 module.exports.help = MESSAGES.Commandes.Admin.RESTART;
